a248bc725c1a38adbe4805aef18624ea8ad08af0
Adriano Belfort <adriano.belfort@mercadolivre.com>
2022-02-16 10:39:27 -0300

    Add example of a map over a list of integers

--- src/main/kotlin/Lambdas.kt
+++ src/main/kotlin/Lambdas.kt
@@ -20,4 +20,8 @@ fun main() {
     }
 
     println("Sum of squares: $result")      // 90
+
+    val doubledNumbers = listOf(1, 2, 3).map { it * 2 }
+
+    println("Doubled numbers: $doubledNumbers")
 }
